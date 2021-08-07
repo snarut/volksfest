f9567f02398c93bd549102ace11f01752a297d46
Narut Sutakawatin <s.narut@gmail.com>
2021-08-08 00:42:44 +0700

    fix static files

--- volksfest/settings/production.py
+++ volksfest/settings/production.py
@@ -24,7 +24,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
