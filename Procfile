1f673e115fdcf2cb9f74fd763d3a3eea77ef2131
Narut Sutakawatin <s.narut@gmail.com>
2021-08-08 23:37:22 +0700

    fixed conflict from revert

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn volksfest.wsgi --log-file -
--- volksfest/settings/production.py
+++ volksfest/settings/production.py
@@ -1,10 +1,3 @@
-from __future__ import absolute_import, unicode_literals
-
-import os
-
-env = os.environ.copy()
-SECRET_KEY = env['SECRET_KEY']
-
 from .base import *
 
 DEBUG = False
